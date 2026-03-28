9991c9113f0f22e1beb1d31641ef0d481493bffc
deep8819 <100400228+deep8819@users.noreply.github.com>
2026-03-28 19:01:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,7 @@ import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 
-# Load HTML file
 with open("snowflake_pic.html", "r", encoding="utf-8") as f:
     html_content = f.read()
 
-# Render HTML
-components.html(html_content, height=1200, scrolling=True)
+components.html(html_content, height=2000, scrolling=True)
